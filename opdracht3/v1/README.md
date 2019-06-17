dd56f611d57f41835547d5046650b84548257cfb
Feline <feline-96@hotmail.com>
2019-06-17 19:33:23 +0200

    bijna keycode klaar

---
+++ opdracht3/v1/README.md
@@ -0,0 +1,67 @@
+# frontend voor designers - opdracht 3
+
+Voor opdracht 3 ga je een functionaliteit ontwerpen met externe data, en testen. De data is JSON die met een REST API van een externe bron wordt ingeladen met Javascript. Als de data geladen is moeten gebruikers je ontwerp op verschillende manieren kunnen bedienen. Verschillende states zijn vormgeven en worden op het juiste moment getoond.
+
+Dit is een voorbeeld van het laden van data en er html mee maken:
+[Code demo](https://koopreynders.github.io/frontendvoordesigners/opdracht3/XMLHttpRequest/)
+
+
+## JSON
+JSON van de movies data staat ook hier:
+deze uri kun je gebruiken voor een van de use case voor opdracht 3.
+[https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json](https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json)
+
+De JSON data is ook los, per film of genre of actors te gebruiken. Check die [hier](https://github.com/KoopReynders/frontendvoordesigners/tree/master/opdracht3/json).
+
+Dit is een voorbeeld van het laden van de film data:
+[Code demo](https://koopreynders.github.io/frontendvoordesigners/opdracht3/v1/)
+
+
+Usbability testing
+
+Mijn onderzoeks vragen:
+1. Snap je wat de bedoeling is?
+2. Is de site duidelijk voor jou?
+3. Wat zou je graag anders zien?
+
+
+Testpersonen
+Tom (21)
+Melanie (23)
+Marjon (53)
+Rene (55)
+
+
+Uitkomsten
+
+Tom
+1. Snap je wat de bedoeling is?
+Ik moet op de button drukken om data te laden.
+2. Is de site duidelijk voor jou?
+Ja heel duidelijk.
+3. Wat zou je graag anders zien?
+Andere lettertypes
+
+Melanie
+1. Snap je wat de bedoeling is?
+Ik snap alleen niet zo goed waarom de recenties doorlopen qua getallen maar misschien waa dat de bedoeling?
+2. Is de site duidelijk voor jou?
+Jahoor is hartstikke duidelijk. Er staat “laad films” dus dan denk ik meteen: ik moet op die knop drukken om de films te laden. Dit zijn websites die voor mensen zoals ik gemaakt zouden moeten worden, lekker simpel, maar wel genoeg informatie!
+3. Wat zou je graag anders zien?
+nee lekker simpel houden
+
+Marjon
+1. Snap je wat de bedoeling is?
+Ja ik denk dat ik op het icoontje moet drukken.
+2. Is de site duidelijk voor jou?
+Ja de site is heel duidelijk.
+3. Wat zou je graag anders zien?
+Nee helemaal goed zo.
+
+Rene
+1. Snap je wat de bedoeling is?
+Ja ik moet op de button 'laad films' drukken.
+2. Is de site duidelijk voor jou?
+Ja prima duidelijk voor mij.
+3. Wat zou je graag anders zien?
+Meer contrast het is moeilijk te lezen.
--- opdracht3/v1/script.js
+++ opdracht3/v1/script.js
@@ -19,99 +19,123 @@ request.send();
 
 request.onload = function () {
     var movies = request.response;
-
 }
 
-function populateHeader(jsonObj) {
-    var myH1 = document.createElement('h1');
-    myH1.textContent = jsonObj.title;
-    section.appendChild(myH1);
-
-    var myPara = document.createElement('p');
-    myPara.textContent = jsonObj['plot'];
-    section.appendChild(myPara);
-
-    var myImg = document.createElement('img');
-    myImg.src = jsonObj['cover'];
-    section.appendChild(myImg);
-}
-
-function showMovies(jsonObj) {
-    var recenties = jsonObj['reviews'];
-
-    for (var i = 0; i < recenties.length; i++) {
-        var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
-        var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myList = document.createElement('ul');
-
-        myH2.textContent = 'Recentie ' + recenties[i].id;
-        myPara1.textContent = 'Score: ' + recenties[i].score;
-
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
-        myArticle.appendChild(myList);
-
-        section.appendChild(myArticle);
-    }
-}
-
-//https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-//https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
-function loadimagesmetXHR() {
-    loaderElement.classList.remove('hide');
-    var request = new XMLHttpRequest();
-    request.open('get', uri);
-    request.responseType = 'json';
-    //request.responseType = 'text'; // now we're getting a string!
-    request.send();
-
-    request.addEventListener("load", function () {
-        console.log("request is geladen: ", request.response);
-        //als de data geladen is, mag de loader weg:
-        loaderElement.classList.add('hide');
-        //functie aanroepen om van de data html te maken
-        showMovies(request.response);
-    });
-    //  request.onload = function() {
-    //      console.log("request.onload: ",request.response);
-    //    }
-    request.timeout = 10000; // time in milliseconds
-    request.ontimeout = function (e) {
-        // XMLHttpRequest timed out. Do something here.
-        console.log("ontimeout: " + request.timeout + ", het laden duurt te lang !", e);
-    };
-    request.onerror = function () {
-        console.log('Fetch Error', request.status);
-    };
-} //end: function loadimagesmetXHR();
 
 
-//loader feedback eerst onzichtbaar maken
-//loaderElement.classList.add('hide');
-//actie
-button = document.querySelector('button');
-button.onclick = function () {
-    //loaderElement.classList.remove('hide'); //loader wordt zichtbaar
-    //this.classList.add('hide'); //button mag weg
-    //section.innerHTML = ""; //main leeghalen. just in case
-    var movies = request.response;
-    loadimagesmetXHR();
-    populateHeader(movies[0]);
-    showMovies(movies[0]);
-    populateHeader(movies[1]);
-    showMovies(movies[1]);
-    populateHeader(movies[2]);
-    showMovies(movies[2]);
-    populateHeader(movies[3]);
-    showMovies(movies[3]);
-    populateHeader(movies[4]);
-    showMovies(movies[4]);
-    populateHeader(movies[5]);
-    showMovies(movies[5]);
-    console.log(movies);
-}; //end: button.onclick
+////TEST Keydown
+
+
+ document.onkeydown = function (event){
+     
+     if (event.keyCode == 70) { // letter F
+     var request = new XMLHttpRequest();
+         request.open('get', uri);
+         request.responseType = 'json';
+         request.send();
+         console.log("loadimagesmetXHR", request);
+         
+         request.addEventListener("load", function (){
+             var data = request.response;
+             console.log("request is geladen: ", request.response);
+             
+             populateHeader(data);
+         });
+     }}
+
+
+        ////TEST Keydown
+
+
+        function populateHeader(jsonObj) {
+            var myH1 = document.createElement('h1');
+            myH1.textContent = jsonObj.title;
+            section.appendChild(myH1);
+
+            var myPara = document.createElement('p');
+            myPara.textContent = jsonObj['plot'];
+            section.appendChild(myPara);
+
+            var myImg = document.createElement('img');
+            myImg.src = jsonObj['cover'];
+            section.appendChild(myImg);
+        }
+
+        function showMovies(jsonObj) {
+            var recenties = jsonObj['reviews'];
+
+            for (var i = 0; i < recenties.length; i++) {
+                var myArticle = document.createElement('article');
+                var myH2 = document.createElement('h2');
+                var myPara1 = document.createElement('p');
+                var myPara2 = document.createElement('p');
+                var myPara3 = document.createElement('p');
+                var myList = document.createElement('ul');
+
+                myH2.textContent = 'Recentie ' + recenties[i].id;
+                myPara1.textContent = 'Score: ' + recenties[i].score;
+
+                myArticle.appendChild(myH2);
+                myArticle.appendChild(myPara1);
+                myArticle.appendChild(myPara2);
+                myArticle.appendChild(myPara3);
+                myArticle.appendChild(myList);
+
+                section.appendChild(myArticle);
+            }
+        }
+
+        //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+        //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+        function loadimagesmetXHR() {
+            loaderElement.classList.remove('hide');
+            var request = new XMLHttpRequest();
+            request.open('get', uri);
+            request.responseType = 'json';
+            //request.responseType = 'text'; // now we're getting a string!
+            request.send();
+
+            request.addEventListener("load", function () {
+                console.log("request is geladen: ", request.response);
+                //als de data geladen is, mag de loader weg:
+                loaderElement.classList.add('hide');
+                //functie aanroepen om van de data html te maken
+                showMovies(request.response);
+            });
+            //  request.onload = function() {
+            //      console.log("request.onload: ",request.response);
+            //    }
+            request.timeout = 10000; // time in milliseconds
+            request.ontimeout = function (e) {
+                // XMLHttpRequest timed out. Do something here.
+                console.log("ontimeout: " + request.timeout + ", het laden duurt te lang !", e);
+            };
+            request.onerror = function () {
+                console.log('Fetch Error', request.status);
+            };
+        } //end: function loadimagesmetXHR();
+
+
+        //loader feedback eerst onzichtbaar maken
+        //loaderElement.classList.add('hide');
+        //actie
+        button = document.querySelector('button'); button.onclick = function () {
+            //loaderElement.classList.remove('hide'); //loader wordt zichtbaar
+            //this.classList.add('hide'); //button mag weg
+            //section.innerHTML = ""; //main leeghalen. just in case
+            var movies = request.response;
+            loadimagesmetXHR();
+            populateHeader(movies[0]);
+            showMovies(movies[0]);
+            populateHeader(movies[1]);
+            showMovies(movies[1]);
+            populateHeader(movies[2]);
+            showMovies(movies[2]);
+            populateHeader(movies[3]);
+            showMovies(movies[3]);
+            populateHeader(movies[4]);
+            showMovies(movies[4]);
+            populateHeader(movies[5]);
+            showMovies(movies[5]);
+            console.log(movies);
+        }; //end: button.onclick
